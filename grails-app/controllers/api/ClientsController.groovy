58a5c6b451b84d50b27c57834ff831956ca0eb3b
Dario Seminara <darios3@gmail.com>
2016-03-29 21:14:56 -0300

    [api] completado endpoint /clients

--- grails-app/controllers/api/ClientsController.groovy
+++ grails-app/controllers/api/ClientsController.groovy
@@ -5,10 +5,25 @@ import grails.converters.JSON
 class ClientsController {
 
     def index() { 
+    	def offset = params.offset
+    	def limit = params.limit
+
+    	if (!limit) limit = "10"
+    	if (!offset) offset = "0"
+
+    	limit = limit.toInteger()
+    	offset = offset.toInteger()
+
+    	def total = ordertracker.Client.count()
+    	def clients = ordertracker.Client.findAll([max:limit, offset:offset])
+
     	def result = [
-    		'results': [1,2,3,4],
-    		'a':1,
-    		'b':2
+    		'results': clients,
+    		'paging': [
+    			'total': total,
+    			'offset': offset,
+    			'limit': limit
+    		]
     	]
     	render result as JSON
     }
